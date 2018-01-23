3650446f2b3eca3403bba6841f9573898a8d54e7
imgr4y <thisisgr4y@gmail.com>
2018-01-22 19:35:27 -0700

    fixed for real this time

--- src/javascript/pages/Oscp.js
+++ src/javascript/pages/Oscp.js
@@ -11,7 +11,7 @@ export default class Oscp extends React.Component {
       <section className="container fadeInUp">
         <div>
           <p>organized list for those interested in following my study procress:</p>
-          <h6><Link to={'/post/jan0818'}> day 1</Link></h6>
+          <h6><Link to={'/post/jan2218'}> day 1</Link></h6>
           <h6><Link to={'/post/nov0817'}> day 2</Link></h6>
           <h6><Link to={'/post/nov0917'}> day 3</Link></h6>
 
--- src/javascript/utils/posts.js
+++ src/javascript/utils/posts.js
@@ -9,14 +9,14 @@ import Nov1317 from '../posts/nov2017/Nov1317';
 
 
 export const recentPosts = [
-  {
-    title: 'day 6',
-    date: '11.13.2017',
-    loadComponent: createFactory(Nov1317),
-    slug: 'nov1317',
-    order: 2,
-    properties: { className: 'white' }
-  },
+  // {
+  //   title: 'day 6',
+  //   date: '11.13.2017',
+  //   loadComponent: createFactory(Nov1317),
+  //   slug: 'nov1317',
+  //   order: 2,
+  //   properties: { className: 'white' }
+  // },
   {
     title: 'day 1: intro',
     date: '01.22.2018',
@@ -30,45 +30,45 @@ export const recentPosts = [
 
 
 export default [
-  {
-    title: 'day 5',
-    date: '11.11.2017',
-    loadComponent: createFactory(Nov1117),
-    slug: 'nov1117',
-    order: 4,
-    properties: { className: 'white' }
-  },
-  {
-    title: 'day 4',
-    date: '11.10.2017',
-    loadComponent: createFactory(Nov1017),
-    slug: 'nov1017',
-    order: 3,
-    properties: { className: 'white' }
-  },
-  {
-    title: 'day 3',
-    date: '11.09.2017',
-    loadComponent: createFactory(Nov0917),
-    slug: 'nov0917',
-    order: 0,
-    properties: { className: 'grey' }
-  },
-  {
-    title: 'day 2',
-    date: '11.08.2017',
-    loadComponent: createFactory(Nov0817),
-    slug: 'nov0817',
-    order: 1,
-    properties: { className: 'white' }
-  },
   // {
-  //   title: 'day 1: intro',
-  //   date: '01.22.2017',
-  //   loadComponent: createFactory(Jan2218),
-  //   slug: 'jan2218',
-  //   order: 2,
-  //   properties: { className: 'black' }
+  //   title: 'day 5',
+  //   date: '11.11.2017',
+  //   loadComponent: createFactory(Nov1117),
+  //   slug: 'nov1117',
+  //   order: 4,
+  //   properties: { className: 'white' }
   // },
+  // {
+  //   title: 'day 4',
+  //   date: '11.10.2017',
+  //   loadComponent: createFactory(Nov1017),
+  //   slug: 'nov1017',
+  //   order: 3,
+  //   properties: { className: 'white' }
+  // },
+  // {
+  //   title: 'day 3',
+  //   date: '11.09.2017',
+  //   loadComponent: createFactory(Nov0917),
+  //   slug: 'nov0917',
+  //   order: 0,
+  //   properties: { className: 'grey' }
+  // },
+  // {
+  //   title: 'day 2',
+  //   date: '11.08.2017',
+  //   loadComponent: createFactory(Nov0817),
+  //   slug: 'nov0817',
+  //   order: 1,
+  //   properties: { className: 'white' }
+  // },
+  {
+    title: 'day 1: intro',
+    date: '01.22.2017',
+    loadComponent: createFactory(Jan2218),
+    slug: 'jan2218',
+    order: 2,
+    properties: { className: 'black' }
+  },
 ]
 
